bbd9af334a7ef20dd41c03649ae76a67f11e392b
joshuaflanagan <joshuaflanagan@gmail.com>
2010-05-13 21:27:34 -0500

    Add help links next to all inputs

--- src/MyApp/MyAppHtml.cs
+++ src/MyApp/MyAppHtml.cs
@@ -4,5 +4,13 @@ namespace MyApp
 {
     public class MyAppHtml : HtmlConventionRegistry
     {
+        public MyAppHtml()
+        {
+            Editors.Always.Modify((field, tag) =>
+            {
+                var helpLink = new HelpLinkTag(field.Accessor.Name);
+                tag.Next = helpLink;
+            });
+        }
     }
 }
